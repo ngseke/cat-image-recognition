9c7d46a04e2db01046b3166b1aff3c8bde0580ce
Huang Xingqiao <a92304a92304@gmail.com>
2019-05-02 20:12:14 +0800

    載入圖片並以image物件儲存。

--- app.vue
+++ app.vue
@@ -5,8 +5,8 @@ div
       a.navbar-brand(href='#') 🐱
       #navbar.navbar-collapse
         ul.navbar-nav.mr-auto
-          li.nav-item: a.nav-link(href='#' @click='page = `train`') Train
-          li.nav-item: a.nav-link(href='#' @click='page = `recognition`') Recognition
+          li.nav-item(v-for='i in navbar')
+            a.nav-link(href='#' @click='page = i.page' :class='getNavbarClass(i.page)') {{ i.name }}
   main.container.mt-3
     Train(v-if='page === `train`')
     Recognition(v-else-if='page === `recognition`')
@@ -21,10 +21,21 @@ export default Vue.extend({
   data () {
     return {
       page: `train`,
+      navbar: [
+        { page: `train`, name: `Train` },
+        { page: `recognition`, name: `Recognition` }
+      ]
     }
   },
   mounted () {
 	},
+  methods: {
+    getNavbarClass (_) {
+      return {
+        active: this.page === _
+      }
+    }
+  },
 	components: {
 		Train,
     Recognition,
--- vue/Train.vue
+++ vue/Train.vue
@@ -5,10 +5,15 @@ div
     .form-group
       .input-group.mb-3
         .custom-file
-          input#inputGroupFile.custom-file-input(type='file' )
+          input#inputGroupFile.custom-file-input(type='file' multiple accept="image/gif, image/jpeg, image/png" @change="fileChange")
           label.custom-file-label(for='inputGroupFile') Choose file
         .input-group-append
           span.input-group-text() Upload
+  .container-fluid
+    .row
+      .col-12 loaded {{ images.length }} images
+      .col-3.col-md-2.mb-1(v-for='i in images')
+        img.img-fluid(:src='i.src')
 </template>
 
 <script>
@@ -18,10 +23,27 @@ import model from '../assets/model'
 export default Vue.extend({
   data () {
     return {
+      file: null,
+      images: [],
     }
   },
   mounted () {
   },
+  methods: {
+    fileChange (event) {
+      const files = event.target.files
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i]
+        const url = URL.createObjectURL(file)
+        const img = new Image()
+
+        img.src = url
+        img.onload = () => {
+        }
+        this.images.push(img)
+      }
+    },
+  }
 })
 </script>
 
